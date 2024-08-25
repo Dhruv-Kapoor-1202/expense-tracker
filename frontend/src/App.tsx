f5921cdb2c1ae76bcd884e1bf35260fbc57b0fe6
Dhruv-Kapoor-1202 <dhruv1690.be21@chitkara.edu.in>
2024-08-25 22:32:58 +0530

    build: initialize frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <h1>Vite + React</h1>
+      <div className="">
+        <button onClick={() => setCount((count) => count + 1)} className='w-20 p-2 capitalize bg-gray-900 rounded-xl text-gray-50'>
+          up
+        </button>
+        <p>{count}</p>
+        <button onClick={() => setCount((count) => count - 1)} className='w-20 p-2 capitalize bg-gray-900 rounded-xl text-gray-50'>
+          down
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
