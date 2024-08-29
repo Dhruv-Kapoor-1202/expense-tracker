9a3a616538a04af3c02ac52ad493dd8ea303907e
Dhruv-Kapoor-1202 <dhruv1690.be21@chitkara.edu.in>
2024-08-30 00:21:13 +0530

    Update Dockerfile
    Timestamp: 52:53

--- server/app.ts
+++ server/app.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { expensesRoute } from "./routes/expenses";
 import { serveStatic } from "hono/bun";
+import { handle } from "hono/vercel";
 
 const app = new Hono();
 
@@ -11,5 +12,8 @@ app.route("/api/expenses", expensesRoute);
 
 app.get("*", serveStatic({ root: "./frontend/dist" }));
 app.get("*", serveStatic({ path: "./frontend/dist/index.html" }));
+
+export const GET = handle(app);
+export const POST = handle(app);
 
 export default app;
